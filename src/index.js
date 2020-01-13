fd7ea925d5676aafe433f2cb6b13afdc56fb981c
Heath <hushijinde@126.com>
2020-01-13 21:56:47 +0800

    feature: 添加坐标;加粗历史记录选择;历史记录排序;高亮三连棋子;平局提醒

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ import './index.css';
 
 function Square(props) {
   return (
-    <button className="square" onClick={props.onClick}>
+    <button className={props.style} onClick={props.onClick}>
       {props.value}
     </button>
   )
@@ -39,7 +39,13 @@ class Board extends React.Component {
     })
   } */
   renderSquare(i) {
-    return <Square value={this.props.squares[i]} onClick={() => this.props.onClick(i)} />;
+    let style
+    if (this.props.line.indexOf(i) !== -1) {
+      style = 'square pink'
+    } else {
+      style = 'square'
+    }
+    return <Square style={style} value={this.props.squares[i]} onClick={() => this.props.onClick(i)} />;
   }
 
   render() {
@@ -79,22 +85,38 @@ class Game extends React.Component {
     super(props)
     this.state = {
       history: [{
-        squares: Array(9).fill(null)
+        num: 0,
+        squares: Array(9).fill(null),
+        coordinate: [0, 0]
       }],
       xIsNext: true,
       stepNumber: 0,
+      order: true,
     }
   }
 
   handleClick(i) {
+    const coordinate = [
+      [1, 1],
+      [2, 1],
+      [3, 1],
+      [1, 2],
+      [2, 2],
+      [3, 2],
+      [1, 3],
+      [2, 3],
+      [3, 3]
+    ]
     const history = this.state.history.slice(0, this.state.stepNumber + 1)
     const current = history[history.length - 1]
     const squares = current.squares.slice()
 
-    if (calculateWinner(squares) || squares[i])  return
+    if (calculateWinner(squares).winner || squares[i]) {
+      return
+    }
     squares[i] = this.state.xIsNext ? 'X' : 'O'
     this.setState({
-      history: history.concat([{squares}]),
+      history: history.concat([{num: history.length, squares, coordinate: coordinate[i]}]),
       xIsNext: !this.state.xIsNext,
       stepNumber: history.length,
     })
@@ -107,36 +129,54 @@ class Game extends React.Component {
     })
   }
 
+  handleOrder() {
+    const history = this.state.history.slice()
+    this.setState({
+      order: !this.state.order,
+      history: history.reverse(),
+    })
+  }
+
   render() {
+    const orderStr = this.state.order ? '升序' : '降序'
     const history = this.state.history
     // const current = history[history.length - 1]
     const current = history[this.state.stepNumber]
-    const winner = calculateWinner(current.squares)
+    const winner = calculateWinner(current.squares).winner
 
     const moves = history.map((step, move) => {
-      const desc = move ? 'Go to move #' + move : 'Go to game start'
+      const desc = step.num ? `Go to move #${step.num} (${step.coordinate})` : `Go to game start${step.coordinate})`
       return (
         <li key={move}>
-          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+          <button className={move === this.state.stepNumber ? 'active' : ''} onClick={() => this.jumpTo(move)}>{desc}</button>
         </li>
       )
     })
 
-    let status
+    let status, line
+
     if (winner) {
       status = 'Winner: ' + winner
+      line = calculateWinner(current.squares).line
     } else {
       status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O')
+      line = []
+      if (this.state.stepNumber >= 9) {
+        alert('平局')
+      }
     }
 
     return (
       <div className="game">
         <div className="game-board">
-          <Board squares={current.squares} onClick={(i) => this.handleClick(i)} />
+          <Board line={line} squares={current.squares} onClick={(i) => this.handleClick(i)} />
         </div>
         <div className="game-info">
           <div>{status}</div>
           <ol>{moves}</ol>
+        </div>
+        <div>
+          <button onClick={() => this.handleOrder()}><strong>{orderStr}</strong>排列历史记录</button>
         </div>
       </div>
     );
@@ -164,8 +204,14 @@ function calculateWinner(squares) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      return {
+        winner: squares[a],
+        line: lines[i]
+      }
     }
   }
-  return null;
+  return {
+    winner: null,
+    line: []
+  }
 }
